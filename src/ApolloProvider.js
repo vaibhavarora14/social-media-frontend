8ff28797837ba973f454ad8734f1abe85bbdec45
Vaibhav Arora <varora1406@gmail.com>
2021-04-18 23:43:58 +0530

    feat: 🎸 edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@ap
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://arcane-reaches-65165.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
